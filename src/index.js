42061748053d1cf026fdc2891306fd4ee6d05ceb
Nana <nka2122@columbia.edu>
2022-04-11 21:01:56 -0400

    update scss to use math.div instead of slash

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import "./scss/style.default.scss";
 
-import App from './components/App';
+import App from './pages/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
-import Header from './Header';
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
+import Header from '../components/Header';
 import history from '../history';
 
 const App = () => {
